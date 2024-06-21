c7cad4e0ea0646c5f9242a3254013bc182faa06d
wslmacieira <wslmacieira@outlook.com>
2024-06-21 12:33:00 -0300

    ci: changes eslint and add prettier scripts

--- index.ts
+++ index.ts
@@ -1,24 +1,3 @@
-import { sendHello } from '@/infrastructure/services/teste'
+import { UserHandler } from '@/presentation/handlers/User'
 
-export async function handler(event: unknown) {
-  await new Promise((resolve) => setTimeout(resolve, 500))
-  const name = 'wagner'
-
-  const hello = sendHello()
-  const response = {
-    statusCode: 200,
-    headers: {
-      'Access-Control-Allow-Headers': 'Content-Type',
-      'Access-Control-Allow-Origin': '*',
-      'Access-Control-Allow-Methods': 'OPTIONS,POST,GET',
-    },
-    body: JSON.stringify({
-      message: 'Go Serverless v1.0! Your function executed successfully!',
-      hello,
-      name,
-      input: event,
-    }),
-  }
-
-  return response
-}
+export const handler = new UserHandler().handler
